346753dad067fde0a37c3b54d60e4fca5d6caf58
norip <piron98@gmail.com>
2019-08-14 21:52:30 +0200

    1.2

--- spider.py
+++ spider.py
@@ -18,13 +18,13 @@ class Spider:
     crawled = set()
 
 
-    def __init__(self,project_name,base_url,domain_name):
+    def __init__(self,project_name, base_url, domain_name):
         Spider.project_name = project_name
         Spider.base_url = base_url
-        Spider.base_url.domain_name = domain_name
+        Spider.domain_name = domain_name
         Spider.queue_file = Spider.project_name + '/queue.txt'
         Spider.crawled_file = Spider.project_name + '/crawled.txt'
-        self.boot()
+        self.boot(self)
         self.crawl_page('First spider', Spider.base_url)
 
     @staticmethod
@@ -52,7 +52,7 @@ class Spider:
             if response.getheader('Content-Type') == 'text/html':
                 html_bytes = response.read()
                 html_string = html_bytes.decode("utf-8")
-            finder = LinkFinder(Spider.base_url,page_url)
+            finder = LinkFinder(Spider.base_url, page_url)
             finder.feed(html_string)
         except:
             print("Error: can not crawl page")
