9eab750094e262f0f4f7e3a84162193cc7dfacce
norip <piron98@gmail.com>
2019-08-14 15:55:57 +0200

    update 0.1.3

--- spider.py
+++ spider.py
@@ -1,5 +1,27 @@
+from urllib.request import urlopen
+
+from linkfinder import LinkFinder
+
+from general import *
+
 
 
 class Spider:
 
-    test_test1d
+    # class variab (shared all instances)
+    project_name = ''
+    base_url = ''
+    domain_name = ''
+    queue_file = ''
+    crawled_file = ''
+    queue = set()
+    crawled = set()
+
+
+    def __init__(self,project_name,base_url,domain_name):
+        Spider.project_name = project_name
+        Spider.base_url = base_url
+        Spider.base_url.domain_name = domain_name
+        Spider.queue_file = Spider.project_name + '/queue.txt'
+        Spider.crawled_file = Spider.project_name + '/crawled.txt'
+
