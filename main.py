a4e833aded0831f0b8c31b6785dbc55e08d6ae94
norip <piron98@gmail.com>
2019-08-15 13:01:35 +0200

    1.4

--- main.py
+++ main.py
@@ -12,3 +12,38 @@ CRAWLED_FILE = PROJECT_NAME + "/crawled.txt"
 NUMBER_OF_THREADS = 8
 queue = Queue()
 Spider(PROJECT_NAME, HOMEPAGE, DOMAIN_NAME)
+
+
+#create worker thereads ( will die when main exits)
+
+def create_workers():
+    for _ in range(NUMBER_OF_THREADS):
+        t = threading.Thread(target=work)
+        t.daemon = True # ???
+        t.start()
+
+#Do the next job in the queue
+def work():
+    while True:
+        url = queue.get()
+        Spider.crawl_page(threading.current_thread().name,url)
+        queue.task_done()
+
+
+
+
+# Each queued link is a new job:
+def create_jobs():
+    for link in file_to_set(QUEUE_FILE):
+        queue.put(link)
+    queue.join()
+    crawl()
+
+
+
+# CHeck if there are items in the queue if so crawl them
+def crawl():
+    queue_links = file_to_set(QUEUE_FILE)
+    if len(queue_links) > 0:
+        print(str(len(queue_links))+ 'links in the queue')
+        create_jobs()
